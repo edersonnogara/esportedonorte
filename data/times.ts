aef1eadd41a0eee79f5a84f2899fac3afa2935cf
Ederson Nogara <82554275+edersonnogara@users.noreply.github.com>
2026-04-10 14:19:29 -0400

    Add files via upload

--- data/times.ts
+++ data/times.ts
@@ -1,14 +1,91 @@
 export type Time = {
+  id: string;
   nome: string;
+  estado: string;
   logo: string;
+  estadio: string;
+  competicoes: {
+    nome: string;
+    grupo?: string;
+  }[];
 };
 
+export function getEstadios() {
+  const lista = times.map(t => t.estadio);
+
+  return [...new Set(lista)]; // remove duplicados
+}
+
 export const times: Time[] = [
-    { nome: "Porto Velho", logo: "/times/PortoVelhoEC.png" },
-    { nome: "Ji-Paraná", logo: "/times/JiParana1.png" },
-    { nome: "Guaporé", logo: "/times/Guaporé_FC.png" },
-    { nome: "União Cacoalense", logo: "/times/UniaoCacoalense.png" },
-    { nome: "Rondoniense", logo: "/times/RondonienseSocialClube.png" },
-    { nome: "Barcelona-RO", logo: "/times/BarcelonaRO.png" },
-    { nome: "Genus", logo: "/times/SCGenus.png" },
+  {
+    id: "porto-velho",
+    nome: "Porto Velho",
+    estado: "RO",
+    logo: "/times/PortoVelhoEC.png",
+    estadio: "Aluizão",
+    competicoes: [{ nome: "Rondoniense" }, { nome: "Copa Verde", grupo: "B" }],
+  },
+  {
+    id: "Ji-Paraná",
+    nome: "Ji-Paraná",
+    estado: "RO",
+    logo: "/times/JiParana1.png",
+    estadio: "Biancão",
+    competicoes: [{ nome: "Rondoniense" }],
+  },
+  {
+    id: "guapore",
+    nome: "Guaporé",
+    estado: "RO",
+    logo: "/times/Guaporé_FC.png",
+    estadio: "Cassolão",
+    competicoes: [{ nome: "Rondoniense" }, { nome: "Copa Verde", grupo: "A" }],
+  },
+  {
+    id: "genus",
+    nome: "Genus",
+    estado: "RO",
+    logo: "/times/SCGenus.png",
+    estadio: "Aluizão",
+    competicoes: [{ nome: "Rondoniense" } ],
+  },
+  {
+    id: "rondoniense",
+    nome: "Rondoniense",
+    estado: "RO",
+    logo: "/times/RondonienseSocialClube.png",
+    estadio: "Aluizão",
+    competicoes: [{ nome: "Rondoniense" }],
+  },
+  {
+    id: "barcelona-ro",
+    nome: "Barcelona-RO",
+    estado: "RO",
+    logo: "/times/BarcelonaRO.png",
+    estadio: "Aluizão",
+    competicoes: [{ nome: "Rondoniense" }],
+  },
+  {
+    id: "uniao-cacoalense",
+    nome: "União Cacoalense",
+    estado: "RO",
+    logo: "/times/UniaoCacoalense.png",
+    estadio: "Aglair Tonelli",
+    competicoes: [{ nome: "Rondoniense" }],
+  },
+
+//times da copa verde
+
+{
+    id: "Nacional-AM",
+    nome: "Nacional-AM",
+    estado: "AM",
+    logo: "/times/Nacional_FC_Amazonas.png",
+    estadio: "Aglair Tonelli",
+    competicoes: [{ nome: "Copa Verde", grupo: "A" }],
+  },
+
+
+
+
 ];
